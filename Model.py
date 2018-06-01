496e390842f53008776403247ee92df28a1fc846
徐天诚 <xutiancheng@Campusnetwork.net.hziee.edu.cn>
2018-06-01 21:20:43 +0800

    HWOK

--- Model.py
+++ Model.py
@@ -1,48 +1,59 @@
 import numpy as np
+from radar import decorater
 
 class HammersteinWiener:
-
     B = []
     b = []
     h = []
-    def __init__(self,B,b,h):
+
+    def __init__(self, B, b, h):
         self.B = B
         self.b = b
         self.h = h
-
+    @decorater
     def run(self, x):
         v = self.f(x)
         u = self.H(v)
         y = self.g(u)
         return y
 
-
     def f(self, x):
-        #x = np.array(x)
+        # x = np.array(x)
         y = np.zeros(x.shape)
-        for n,_x in enumerate(x):
+        for n, _x in enumerate(x):
             temp = 0
             for i in range(len(self.b)):
-                temp += self.b[i]  * pow(_x, 2*i+1)
+                temp += self.b[i] * pow(_x, 2 * i + 1)
             y[n] = temp
         return y
 
     def g(self, x):
-        #x = np.array(x)
+        # x = np.array(x)
         y = np.zeros(x.shape)
-        for n,_x in enumerate(x):
+        for n, _x in enumerate(x):
             temp = 0
             for i in range(len(self.B)):
-                temp += self.B[i]  * pow(_x, 2*i+1)
+                temp += self.B[i] * pow(_x, 2 * i + 1)
             y[n] = temp
         return y
 
     def H(self, x):
-        #x = np.array(x)
+        # x = np.array(x)
         y = np.zeros(x.shape)
-        for n,_x in enumerate(x):
+        for n, _x in enumerate(x):
             temp = 0
             for i in range(len(self.h)):
-                temp += self.h[i] * x[n-i]
+                temp += self.h[i] * x[n - i]
             y[n] = temp
         return y
+    @decorater
+    def find_way(self, x, y):
+        est = HammersteinWiener(B=[0 for _ in range(len(self.B))], b=[0 for _ in range(len(self.b))],
+                                h=[0 for _ in range(len(self.h))])
+
+    def __str__(self):
+        ans = ""
+        ans += "\033[0;33mb =\033[0m {}\n".format(self.b)
+        ans += "\033[0;33mB =\033[0m {}\n".format(self.B)
+        ans += "\033[0;33mh =\033[0m {}\n".format(self.h)
+        return ans
--- main.py
+++ main.py
@@ -7,11 +7,7 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     radar_list = [BaseRadar(random.randint(0, 200), random.randint(0, 200)) for i in range(10)]
-    HW = Model.HammersteinWiener(B = [1,0,0], b = [1,0,0], h = [1,0,0])
-    for radar in radar_list[:1]:
+    HW = Model.HammersteinWiener(B=[1, 0, 0], b=[1, 0, 0], h=[1, 0, 0])  ###
+    for radar in radar_list:
         x = radar.trans_wave()
-        plt.figure()
-        plt.plot(HW.run(x))
-        plt.figure()
-        plt.plot(x,color = 'red')
-        plt.show()
+    HW.find_way(0,0)
